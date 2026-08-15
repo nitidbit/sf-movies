d94a004abe4cbb8793e57b2b63e8b7a3e1f40a34
Winston <winston@nitidbit.com>
2026-08-15 00:34:48 -0700

    Adjust time format

--- src/shared/dateFormat.ts
+++ src/shared/dateFormat.ts
@@ -3,12 +3,14 @@ function partGetter(formatter: Intl.DateTimeFormat, date: Date): (type: Intl.Dat
   return (type) => parts.find((part) => part.type === type)?.value ?? "";
 }
 
+// Return e.g. "Sat, Aug 15, 2026"
 export function preferredDateFmt(formatter: Intl.DateTimeFormat, date: Date): string {
   const get = partGetter(formatter, date);
   return `${get("weekday")}, ${get("month")} ${get("day")}, ${get("year")}`;
 }
 
+// Return e.g. "9:30pm, Sat Aug 15"
 export function preferredDateTimeFmt(formatter: Intl.DateTimeFormat, date: Date): string {
   const get = partGetter(formatter, date);
-  return `${get("weekday")}, ${get("month")} ${get("day")}, ${get("hour")}:${get("minute")} ${get("dayPeriod").toLowerCase()}`;
+  return `${get("hour")}:${get("minute")}${get("dayPeriod").toLowerCase()}, ${get("weekday")} ${get("month")} ${get("day")}`;
 }
