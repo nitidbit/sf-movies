b400d1b85bcfef076f714f4bfa6b65e13fb84cba
Winston <winston@nitidbit.com>
2026-08-15 00:04:19 -0700

    Visual tweaks.

--- dev-start.sh
+++ dev-start.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
 cd "$(dirname "$0")"
-npm run dev
+
+npm run dev &
+DEV_PID=$!
+
+# Maybe open page in browser
+if [[ -n "${BROWSER:-}" ]]; then
+  sleep 1
+  open -a $BROWSER http://localhost:4322/sf-movies/
+fi
+
+wait "$DEV_PID"
